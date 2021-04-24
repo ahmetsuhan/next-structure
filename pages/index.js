366f8efab7a62bd3c5dd27e881ea60944c116a3c
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-04-24 05:10:21 +0300

    dataFetching with both getServerSideProps and getStaticProps methods

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,34 @@
 import Layout from "../layout";
-export default function HomePage() {
+import { API_URL } from "@/config/index";
+export default function HomePage({ events }) {
+  //console.log(events);
   return (
     <Layout>
-      <div>HomePAge</div>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}api/events`);
+  const events = await res.json();
+  //console.log(events);
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}api/events`);
+//   const events = await res.json();
+//   //console.log(events);
+//   return {
+//     props: {
+//       events: events,
+//     },
+//     revalidate: 1,
+//   };
+// }
