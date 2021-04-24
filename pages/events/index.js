4debec44a6b37abf6226504e9116e1a81c950173
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-04-24 05:41:24 +0300

    displaying events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,40 @@
 import Layout from "../../layout";
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
+export default function EventsPage({ events }) {
+  console.log(events);
 
-export default function EventsPage() {
   return (
     <Layout>
-      <div>EventsPage</div>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((event) => {
+        return <EventItem key={event.id} event={event} />;
+      })}
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}api/events`);
+  const events = await res.json();
+  //console.log(events);
+  return {
+    props: {
+      events: events.slice(0, 3),
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}api/events`);
+//   const events = await res.json();
+//   //console.log(events);
+//   return {
+//     props: {
+//       events: events,
+//     },
+//     revalidate: 1,
+//   };
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import Layout from "../layout";
 import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
+import Link from "next/link";
 export default function HomePage({ events }) {
-  //console.log(events);
+  console.log(events);
+
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((event) => {
+        return <EventItem key={event.id} event={event} />;
+      })}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
