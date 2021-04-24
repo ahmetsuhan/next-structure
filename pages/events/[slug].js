157a907459b9bd690339430591c24110b57d0bf2
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-04-24 05:56:59 +0300

    dynamic routing with getStaticPaths method

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,47 @@
 import Layout from "../../layout";
+import { API_URL } from "@/config/index";
 
-export default function EventPage() {
+export default function EventPage({ event }) {
   return (
     <Layout>
-      <div>EventPage</div>
+      <h1>{event.name}</h1>
     </Layout>
   );
 }
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths: paths,
+    fallback: true,
+  };
+}
+export async function getStaticProps({ params: { slug } }) {
+  //console.log(slug);
+
+  const res = await fetch(`${API_URL}api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      event: events[0],
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   //console.log(slug);
+
+//   const res = await fetch(`${API_URL}api/events/${slug}`);
+//   const events = res.json();
+//   return {
+//     props: {
+//       event: events[0],
+//     },
+//   };
+// }
