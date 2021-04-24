8170c51ae063a49da570b5401c2813137a5cc8b9
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-04-24 21:17:52 +0300

    add alias for helpers, create cookieParser helper function, getting user events for dashboard

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,31 @@
 import Layout from "../../layout";
+import { parseCookies } from "@helpers/index";
+import { API_URL } from "@/config/index";
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+  console.log(events);
   return (
     <Layout title="User Dashboard">
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  //console.log(token)
+
+  const res = await fetch(`${API_URL}events/me`, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
