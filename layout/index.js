3a00cf7e61c3922d90ba75efd811c8c51811f011
Ahmet Suhan Oka <ahmet.oka@yemeksepeti.com>
2021-04-24 03:42:20 +0300

    give module alias with jsconfig.json

--- layout/index.js
+++ layout/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import styles from "../styles/Layout.module.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import styles from "@/styles/Layout.module.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Layout({ children, title, description, keywords }) {
   return (
